86678c54cafde938549ba820512da7d22e15f677
Devilcore54 <max.mirtschink@wtnet.de>
2014-08-15 17:37:19 +0200

    added item class

---
+++ include/item.h
@@ -0,0 +1,16 @@
+#ifndef ITEM_H
+#define ITEM_H
+
+
+class Item
+{
+    public:
+        Item();
+        virtual ~Item();
+    protected:
+    private:
+        unsigned int id;
+
+};
+
+#endif // ITEM_H
---
+++ src/item.cpp
@@ -0,0 +1,11 @@
+#include "item.h"
+
+Item::Item()
+{
+    //ctor
+}
+
+Item::~Item()
+{
+    //dtor
+}
